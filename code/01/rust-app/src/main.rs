9bd782466d69169f7581fb0ccf9bcbc29781d467
Evian-Zhang <evianzhang1999@163.com>
2023-01-15 19:56:07 +0800

    Add rust app to use wasm library

---
+++ code/01/rust-app/src/main.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+use std::fs;
+use wasmer::{Imports, Instance, Module, Store, TypedFunction};
+
+fn main() -> Result<()> {
+    let wasm_bytes = fs::read("./adder.wasm")?;
+    let mut store = Store::default();
+    let module = Module::new(&store, wasm_bytes)?;
+    let imports = Imports::default();
+    let instance = Instance::new(&mut store, &module, &imports)?;
+    let run_func: TypedFunction<(u32, u32), u32> =
+        instance.exports.get_typed_function(&mut store, "add")?;
+    let sum = run_func.call(&mut store, 1, 2)?;
+    println!("Sum is {sum}");
+    Ok(())
+}
